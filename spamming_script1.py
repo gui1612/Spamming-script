af1a0d5d9ea826e2a5bc6edf9353114ca9f00a55
gui1612 <guialda1234@gmail.com>
2020-11-08 00:38:59 +0000

    Added some features
    Added an infinite spam mode, a timed one and one with discord specific timings

--- spamming_script1.py
+++ spamming_script1.py
@@ -4,31 +4,50 @@ from datetime import datetime
 
 #Variables
 msg = input("Enter the message: ")
-n = input("How many times ?: ")
-delay = input("How much delay do you want? ")
+delay = float(input("How much delay do you want between messages? "))
+
+
 
 def format_time():
+    """
+    Returns the current date as a string with te chosen digits
+    """
     t = datetime.now()
     s = t.strftime('%Y-%m-%d %H:%M:%S.%f')
     return "date: " + s[5:-7] + "-> "
 
-print(format_time())
+def regular_spam(msg, delay):
+    """
+    Automates de keyboard presses of the current value followed by the inputed 
+    msg value and also sets a delay between consequent message
+    """
+    typewrite(format_time() + msg + '\n')
+    # typewrite(msg + '\n')                    #uncomment if you want the spam message without date and time
+    sleep(delay)
 
 
 
-print("t minus")
+choice= input("Do you want a regular spam, infinite spam or instant spam waiting times?[regular, infinite or instant]")
 
-sleep(5)
-count = 5
-while(count != 0):
-	print(count)
-	sleep(1)
-	count -= 1
 
-print("Fire in the hole!!!")
-
-for i in range(0,int(n)):
-    for k in range(9):
-        typewrite(format_time() + msg + '\n')
-        sleep(0.8)
-    sleep(5)               # discord wait time. Fuck discord, all my homies hate discord
+if choice.lower() == "instant":
+    n = int(input("How many times do you want the message to repeat?: "))
+    sleep(5)
+    for i in range(0, n):
+        for k in range(9):
+            regular_spam(msg, delay)
+        sleep(5)               
+    
+    
+if choice.lower() == "regular":
+    n = int(input("How many times do you want the message to repeat?: "))
+    sleep(5)
+    for i in range(0, n):
+        regular_spam(msg, delay)
+        
+        
+if choice.lower() == "infinite":
+    sleep(5)
+    while True:
+        regular_spam(msg, delay)
+   
